29816a29bb2b2fa7b3077fa7b0d8420b9e249b87
Yuichiro Tsuji <t.yuyu.alies@gmail.com>
2024-01-13 20:41:38 +0900

    [Add] /booksのAPIView作成

--- api/books/views.py
+++ api/books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import status
 
 # Create your views here.
+class BookView(APIView):
+    # 書籍一覧を取得
+    def get(self, request, format=None):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/hello/", include("api.hello.urls")),
     path("api/hello_db/", include("api.hello_db.urls")),
+    path("api/books/", include("api.books.urls")),
 ]
