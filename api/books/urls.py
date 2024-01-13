db2f518b10033caeb5b08ac4883eceef778097ea
Yuichiro Tsuji <t.yuyu.alies@gmail.com>
2024-01-13 23:02:19 +0900

    [Add] 書籍の登録、編集、削除処理

--- api/books/urls.py
+++ api/books/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.BookView.as_view()),
+    path("<int:id>/", views.BookView.as_view()),
 ]
--- api/books/views.py
+++ api/books/views.py
@@ -3,11 +3,60 @@ from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 # Create your views here.
 class BookView(APIView):
-    # 書籍一覧を取得
-    def get(self, request, format=None):
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
+    # 書籍の一覧もしくは一意の書籍を取得
+    def get(self, request, id=None, format=None):
+        if id is None:
+            books = Book.objects.all()
+            serializer = BookSerializer(books, many=True)
+        else:
+            book = self.get_book(id)
+            serializer = BookSerializer(book)
         return Response(serializer.data, status.HTTP_200_OK)
+    
+    def get_book(self, pk):
+        try:
+            return Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise NotFound
+    
+    def post(self, request, format=None):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, id, format=None):
+        book = self.get_book(id)
+        serializer = BookSerializer(instance=book, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+        
+    def delete(self, request, id, format=None):
+        book = self.get_book(id)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+class BookDetailView(APIView):
+    # 書籍詳細を取得
+    def get(self, request, pk, format=None):
+        book = Book.objects.get(pk=pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def put(self, request, pk, format=None):
+        book = Book.objects.get(pk=pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
